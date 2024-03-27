a98f2b6dc2903100c741a8685853912e41f362ec
Jaeyonging <wodyd1318@naver.com>
2024-03-27 15:33:25 +0900

    api fixed

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,12 +3,12 @@ let qtestAPI = import.meta.env.VITE_QTEST_KEY
 
 
 export async function FetchQuestions(qtype: string) {
-    return axios.get(`http://www.career.go.kr/inspct/openapi/test/questions?apikey=${qtestAPI}&q=${qtype}`)
+    return axios.get(`https://www.career.go.kr/inspct/openapi/test/questions?apikey=${qtestAPI}&q=${qtype}`)
 }
 
 export async function FetchResult(qtype: string, gender: string, qanswer: string) {
     const startDtm = Date.now().toString()
-    return axios.post('http://www.career.go.kr/inspct/openapi/test/report', {
+    return axios.post('https://www.career.go.kr/inspct/openapi/test/report', {
         apikey: `${qtestAPI}`,
         qestrnSeq: qtype,
         trgetSe: "100209",
