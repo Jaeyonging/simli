39c151f31d3ba26de067b30c1ff6f7f3c93a3319
Jaeyonging <wodyd1318@naver.com>
2024-03-27 15:53:16 +0900

    url

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,12 +6,10 @@ export async function FetchQuestions(qtype: string) {
     return axios.get(`https://www.career.go.kr/inspct/openapi/test/questions?apikey=${qtestAPI}&q=${qtype}`)
 }
 
-const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
-const apiUrl = 'https://www.career.go.kr/inspct/openapi/test/report';
 
 export async function FetchResult(qtype: string, gender: string, qanswer: string) {
     const startDtm = Date.now().toString()
-    return axios.post(corsAnywhereUrl + apiUrl, {
+    return axios.post('https://www.career.go.kr/inspct/openapi/test/report', {
         apikey: `${qtestAPI}`,
         qestrnSeq: qtype,
         trgetSe: "100209",
